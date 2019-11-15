30542e13e79659e97c6504ecac8b4c44a91192fe
KERFAH Issam <i.kerfah@gmail.com>
2019-11-15 22:32:10 +0100

    Initial commit

---
+++ app/src/main/java/com/ikerfah/junction2019/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ikerfah.junction2019
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
