d9f9923ff0e4e6eafbb85c305d912c7de8196655
KERFAH Issam <i.kerfah@gmail.com>
2019-11-16 11:14:10 +0100

    Add databinding to item meal

--- app/src/main/java/com/ikerfah/junction2019/MainActivity.kt
+++ app/src/main/java/com/ikerfah/junction2019/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.ikerfah.junction2019
 
 import android.graphics.Rect
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -11,25 +11,26 @@ import com.ikerfah.junction2019.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var mBinding:ActivityMainBinding
+    private lateinit var mBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         var adapter = MealAdapter(
             arrayListOf(
-                Meal("meal 1"),
-                Meal("meal 2"),
-                Meal("meal 3"),
-                Meal("meal 4"),
-                Meal("meal 5"),
-                Meal("meal 6"),
-                Meal("meal 7"),
-                Meal("meal 8")
+                Meal("meal 1", "8/8 ing", "24 min", "Ready to tyab", "Healthy"),
+                Meal("meal 2", "8/8 ing", "24 min", "Not to tyab", "Not Healthy"),
+                Meal("meal 3", "8/8 ing", "24 min", "Ready to tyab", "Not  Healthy"),
+                Meal("meal 4", "8/8 ing", "10 min", "Not to tyab", "Not  Healthy"),
+                Meal("meal 5", "8/8 ing", "29 min", "Ready to tyab", "Healthy"),
+                Meal("meal 6", "8/8 ing", "20 min", "Not to tyab", "Healthy"),
+                Meal("meal 7", "8/8 ing", "5 min", "Ready to tyab", "Not Healthy"),
+                Meal("meal 8", "8/8 ing", "64 min", "Ready to tyab", "Healthy")
             )
         )
 
-        mBinding.recycler.layoutManager = LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false)
+        mBinding.recycler.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         mBinding.recycler.adapter = adapter
 
         mBinding.recycler.addItemDecoration(object : RecyclerView.ItemDecoration() {
--- app/src/main/java/com/ikerfah/junction2019/Meal.kt
+++ app/src/main/java/com/ikerfah/junction2019/Meal.kt
@@ -1,7 +1,11 @@
 package com.ikerfah.junction2019
 
 data class Meal(
-    var name: String? = null
+    var name: String? = null,
+    var ingredients: String? = null,
+    var duration: String? = null,
+    var state: String? = null,
+    var foodType: String? = null
 
 ) {
 
